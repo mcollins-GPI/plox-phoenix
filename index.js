e15c464ddb8612e3babec02910c78d54497dd2b3
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-12-01 17:07:50 -0500

    Updates to Routes Added oauth security updated error handling added playlist control

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const { Readable } = require('stream');
 const path = require('path');
 require('dotenv').config();
 const dropboxV2Api = require('dropbox-v2-api');
+const open = require('open');
 
 const dropbox = dropboxV2Api.authenticate({
-    token: process.env.token,
     client_id: process.env.client_id,
     client_secret: process.env.client_secret,
+    redirect_uri: 'http://localhost/plox-phoenix/data/oauth',
+    token_access_type: 'offline',
+    state: 'OPTIONAL_STATE_VALUE',
 });
+//generate and visit authorization sevice
+open(dropbox.generateAuthUrl());
 
-function getFolders(artist, album) {
+function getFolders(artist, album, metadata = false) {
     let fileList = [];
     let searchPath = `${artist ? `${album ? `/music/${artist}/${album}` : `/music/${artist}`}` : '/music'}`;
 
@@ -21,17 +26,20 @@ function getFolders(artist, album) {
         dropbox(
             {
                 resource: 'files/list_folder',
-                parameters: { path: searchPath },
+                parameters: { path: searchPath, include_media_info: metadata },
             },
             (err, result, response) => {
                 console.log(err, result, response);
-                fileList = fileList.concat(result.entries);
+                if (err) reject(err);
+                if (result) {
+                    if (result.entries) fileList = fileList.concat(result.entries);
 
-                //download completed
-                if (result.has_more) {
-                    return getMoreFolders(result.cursor);
-                } else {
-                    resolve(fileList);
+                    //download completed
+                    if (result.has_more) {
+                        return getMoreFolders(result.cursor);
+                    } else {
+                        resolve(fileList);
+                    }
                 }
             }
         );
@@ -43,13 +51,15 @@ function getFolders(artist, album) {
                     parameters: { cursor: cursor },
                 },
                 (err, result, response) => {
-                    console.log(err, result, response);
-                    fileList = fileList.concat(result.entries);
+                    if (err) reject(err);
+                    if (result) {
+                        if (result.entries) fileList = fileList.concat(result.entries);
 
-                    if (result.has_more) {
-                        return getMoreFolders(result.cursor);
-                    } else {
-                        resolve(fileList);
+                        if (result.has_more) {
+                            return getMoreFolders(result.cursor);
+                        } else {
+                            resolve(fileList);
+                        }
                     }
                 }
             );
@@ -57,77 +67,113 @@ function getFolders(artist, album) {
     });
 }
 
-async function getFile(searchPath) {
-    // try {
-    //     const downloadStream = dropbox({
-    //         resource: 'files/download',
-    //         parameters: { path: searchPath },
-    //     });
-
-    //     return downloadStream;
-    // } catch (error) {
-    //     console.error(error);
-    // }
-    // return dropbox(
-    //     {
-    //         resource: 'files/download',
-    //         parameters: {
-    //             path: searchPath,
-    //         },
-    //     },
-    //     (err, result, response) => {
-    //         //download completed
-    //     }
-    // ).pipe(fs.createReadStream('./04 the sign.mp3'));
-
-    var readStream = fs.createReadStream(path.join(__dirname, '/audio/believe.mp3'));
-
-    // This will wait until we know the readable stream is actually valid before piping
-    readStream.on('open', function () {
-        // This just pipes the read stream to the response object (which goes to the client)
-        readStream.pipe(res);
-    });
+function getFile(searchPath) {
+    return dropbox(
+        {
+            resource: 'files/download',
+            parameters: {
+                path: searchPath,
+            },
+        },
+        (err, result) => {
+            //download completed
+        }
+    );
 }
 
-fastify.get('/artist', async (request, reply) => {
-    return getFolders().then((response) => {
-        console.log(response);
-        return { artist_list: response };
-    });
-});
-
-fastify.get('/album/:artist', async (request, reply) => {
-    return getFolders(request.params.artist).then((response) => {
-        console.log(response);
-        return { album_list: response };
-    });
-});
-
-fastify.get('/tracks/:artist/:album', async (request, reply) => {
-    return getFolders(request.params.artist, request.params.album).then((response) => {
-        console.log(response);
-        return { track_list: response };
-    });
-});
-
-fastify.get('/track', (request, reply) => {
-    let searchPath = '/music/alkaline trio/crimson [2005]/04 mercy me.mp3';
-
-    console.log(request.headers.path === searchPath);
-    reply.type('application/octet-stream');
-    reply.send(
+function getFileInfo(searchPath) {
+    return new Promise((resolve, reject) => {
         dropbox(
             {
-                resource: 'files/download',
+                resource: 'files/get_metadata',
                 parameters: {
-                    path: request.headers.path,
+                    path: searchPath,
+                    include_deleted: true,
+                    include_has_explicit_shared_members: true,
+                    include_media_info: false,
                 },
             },
             (err, result) => {
-                //download completed
+                if (err) reject(err);
+                resolve(result);
             }
-        )
-    );
+        );
+    });
+}
+
+fastify.get('/oauth', async function (request, reply) {
+    var params = request.query;
+
+    return new Promise((resolve) => {
+        dropbox.getToken(params.code, function (err, response) {
+            // console.log(err);
+            // console.log("user's access_token: ", response.access_token);
+            // console.log("user's refresh_token: ", response.refresh_token);
+            // open('http://localhost/plox-phoenix/library')
+            //call api
+            // dropbox(
+            //     {
+            //         resource: 'users/get_current_account',
+            //     },
+            //     function (err, response) {
+            //         console.log(err);
+            //         resolve(response);
+            //     }
+            // );
+            //or refresh token!
+            // dropbox.refreshToken(response.refresh_token, (err, result) => {
+            //     console.log(err);
+            //     console.log(result);
+            // });
+        });
+    });
+});
+
+fastify.get('/artist', async (request, reply) => {
+    return getFolders()
+        .then((response) => {
+            return { artist_list: response };
+        })
+        .catch((error) => {
+            return { error: error };
+        });
+});
+
+fastify.get('/album', async (request, reply) => {
+    console.log(request.headers.artist);
+    return getFolders(request.headers.artist)
+        .then((response) => {
+            return { album_list: response };
+        })
+        .catch((error) => {
+            return { error: error };
+        });
+});
+
+fastify.get('/tracks', async (request, reply) => {
+    return getFolders(request.headers.artist, request.headers.album, true)
+        .then((response) => {
+            return { track_list: response };
+        })
+        .catch((error) => {
+            return { error: error };
+        });
+});
+
+fastify.get('/track', (request, reply) => {
+    reply.type('application/octet-stream');
+    reply.send(getFile(request.headers.path));
+});
+
+fastify.get('/track-info', async (request, reply) => {
+    getFileInfo(request.headers.path)
+        .then((fileInfo) => {
+            reply.header('Content-Type', 'application/json; charset=utf-8');
+            reply.send(fileInfo);
+        })
+        .catch((error) => {
+            reply.send(error);
+        });
 });
 
 // // LAUNCH SERVER
@@ -138,6 +184,7 @@ fastify.listen(
             console.log(err);
             process.exit(1);
         }
+
         console.info(`Server listening on ${address}`);
     }
 );
--- public/library/js/library.js
+++ public/library/js/library.js
@@ -3,10 +3,12 @@ const artistList = document.getElementById('artist-list');
 const albumList = document.getElementById('album-list');
 const trackList = document.getElementById('track-list');
 const mediaPlayer = document.getElementById('media-player');
+const playList = document.getElementById('play-list');
 const audioController = document.getElementById('audio-controller');
 const artistListControl = new ArtistListControl(artistList);
 const albumListControl = new AlbumListControl(albumList);
 const trackListControl = new TrackListControl(trackList);
+const playlistControl = new PlaylistControl(playList);
 
 function TrackListControl(attachPoint) {
     const self = this;
@@ -19,48 +21,7 @@ function TrackListControl(attachPoint) {
         playAlbumButton.innerHTML = 'play stuff!';
 
         playAlbumButton.addEventListener('click', () => {
-            let i = 0;
-
-            function next() {
-                // Check for last audio file in the playlist
-                if (i === tracks.length - 1) {
-                    i = 0;
-                } else {
-                    i++;
-                }
-
-                // Change the audio element source
-                fetch(`http://localhost/plox-phoenix/data/track`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'text/plain;charset=UTF-8',
-                        path: tracks[i].path_lower,
-                    },
-                }).then((response) => {
-                    response.blob().then((myBlob) => {
-                        const objectURL = URL.createObjectURL(myBlob);
-                        audioController.src = objectURL;
-                    });
-                });
-            }
-
-            // Check if the player is selected
-
-            fetch(`http://localhost/plox-phoenix/data/track`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'text/plain;charset=UTF-8',
-                    path: tracks[i].path_lower,
-                },
-            }).then((response) => {
-                response.blob().then((myBlob) => {
-                    const objectURL = URL.createObjectURL(myBlob);
-                    audioController.src = objectURL;
-                });
-            });
-
-            // Listen for the music ended event, to play the next audio file
-            audioController.addEventListener('ended', next, false);
+            playlistControl.addTracks(tracks);
         });
 
         newAlbumTitle.innerHTML = `${album.name}`;
@@ -112,7 +73,14 @@ function AlbumListControl(attachPoint) {
 
             // console.log(album);
             listItem.addEventListener('click', (event) => {
-                fetch(`http://localhost/plox-phoenix/data/tracks/${artist.name}/${album.name}`)
+                fetch(`http://localhost/plox-phoenix/data/tracks`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'text/plain;charset=UTF-8',
+                        artist: artist.name,
+                        album: album.name,
+                    },
+                })
                     .then((response) => response.json())
                     .then((data) => trackListControl.populate(album, data.track_list));
             });
@@ -143,7 +111,13 @@ function ArtistListControl(attachPoint) {
             const listItem = document.createElement('li');
             listItem.innerHTML = artist.name;
             listItem.addEventListener('click', (event) => {
-                fetch(`http://localhost/plox-phoenix/data/album/${artist.name}`)
+                fetch(`http://localhost/plox-phoenix/data/album`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'text/plain;charset=UTF-8',
+                        artist: artist.name,
+                    },
+                })
                     .then((response) => response.json())
                     .then((data) => albumListControl.populate(artist, data.album_list));
             });
@@ -164,3 +138,171 @@ function ArtistListControl(attachPoint) {
         .then((response) => response.json())
         .then((data) => self.populate(data.artist_list));
 }
+
+function PlaylistControl(attachPoint) {
+    const self = this;
+    this.domNode = document.createElement('table');
+
+    const jsmediatags = window.jsmediatags; // read metadata from track being loaded
+    const trackHeaders = document.createElement('theader');
+    const trackListing = document.createElement('tbody');
+    const numberHeader = document.createElement('td');
+    const titleHeader = document.createElement('td');
+    const artistHeader = document.createElement('td');
+    const albumHeader = document.createElement('td');
+
+    titleHeader.innerHTML = 'Title';
+    artistHeader.innerHTML = 'Artist';
+    albumHeader.innerHTML = 'Album';
+
+    trackHeaders.append(numberHeader, titleHeader, artistHeader, albumHeader);
+
+    this.addTrack = function (track, index) {
+        const trackRow = document.createElement('tr');
+        const number = document.createElement('td');
+        const title = document.createElement('td');
+        const artist = document.createElement('td');
+        const album = document.createElement('td');
+
+        // jsmediatags.read(track, {
+        //     onSuccess: function (tag) {
+        //         console.log(tag);
+        //         title.innerHTML = tag.tags.title;
+        //         artist.innerHTML = tag.tags.artist;
+        //         album.innerHTML = tag.tags.album;
+        //     },
+        //     onError: function (error) {
+        //         console.log(error);
+        //         title.innerHTML = 'no title info';
+        //         artist.innerHTML = 'no artist info';
+        //         album.innerHTML = 'no album info';
+        //     },
+        // });
+
+        // fetch(`http://localhost/plox-phoenix/data/track-info`, {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'text/plain;charset=UTF-8',
+        //         path: track.path_lower,
+        //     },
+        // }).then((response) => {
+        //     console.log(response);
+        //     response.json().then((data) => {
+        //         console.log(data);
+        //     });
+        // });
+
+        number.innerHTML = index + 1;
+        title.innerHTML = track.name;
+
+        trackRow.append(number, title, artist, album);
+        trackListing.append(trackRow);
+
+        if (index === 0) {
+            self.trackNumber = 0;
+            fetch(`http://localhost/plox-phoenix/data/track`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'text/plain;charset=UTF-8',
+                    path: track.path_lower,
+                },
+            }).then((response) => {
+                response.blob().then((myBlob) => {
+                    const objectURL = URL.createObjectURL(myBlob);
+                    audioController.src = objectURL;
+
+                    jsmediatags.read(myBlob, {
+                        onSuccess: function (tag) {
+                            console.log(tag);
+                            // title.innerHTML = tag.tags.title;
+                            // artist.innerHTML = tag.tags.artist;
+                            // album.innerHTML = tag.tags.album;
+                        },
+                        onError: function (error) {
+                            console.log(error);
+                            // title.innerHTML = 'no title info';
+                            // artist.innerHTML = 'no artist info';
+                            // album.innerHTML = 'no album info';
+                        },
+                    });
+                });
+            });
+        }
+    };
+    this.addTracks = function (tracks) {
+        self.clearPlaylist();
+        self.tracks = tracks;
+        tracks.forEach((track, index) => {
+            self.addTrack(track, index);
+        });
+    };
+    this.clearPlaylist = function () {
+        while (trackListing.firstChild) {
+            trackListing.removeChild(trackListing.firstChild);
+        }
+    };
+
+    this.domNode.append(trackHeaders, trackListing);
+
+    attachPoint.append(this.domNode);
+
+    // Listen for the music ended event, to play the next audio file
+    audioController.addEventListener(
+        'ended',
+        () => {
+            // Check for last audio file in the playlist
+            if (self.trackNumber === self.tracks.length - 1) {
+                self.trackNumber = 0;
+            } else {
+                self.trackNumber++;
+            }
+
+            // Change the audio element source
+            fetch(`http://localhost/plox-phoenix/data/track`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'text/plain;charset=UTF-8',
+                    path: self.tracks[self.trackNumber].path_lower,
+                },
+            }).then((response) => {
+                response.blob().then((myBlob) => {
+                    const objectURL = URL.createObjectURL(myBlob);
+                    audioController.src = objectURL;
+                });
+            });
+        },
+        false
+    );
+}
+
+// curl -X POST https://content.dropboxapi.com/2/files/download \
+//     --header "Authorization: Bearer sl.BUJhYyXpUjLencYxQDM3VS6LqdwBHf90mPjpvYK13tCYPE-yMedgYl3jQJ3s-56wjKigIS-TR83xzy4lJOtOUPIuKhwvd1ZOTlXzYjaenaXMpheOqrkd-i9kgmcZR--xyVJD9pBN" \
+//     --header "Dropbox-API-Arg: {\"path\":\"/Homework/math/Prime_Numbers.txt\"}"
+
+//     const rawResponse = await fetch('https://httpbin.org/post', {
+//         method: 'POST',
+//         headers: {
+//           'Accept': 'application/json',
+//           Authorization: Bearer sl.BUJhYyXpUjLencYxQDM3VS6LqdwBHf90mPjpvYK13tCYPE-yMedgYl3jQJ3s-56wjKigIS-TR83xzy4lJOtOUPIuKhwvd1ZOTlXzYjaenaXMpheOqrkd-i9kgmcZR--xyVJD9pBN
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({a: 1, b: 'Textual content'})
+//       });
+//       const content = await rawResponse.json();
+
+//       console.log(content);
+
+const params = new URLSearchParams({
+    code: 'sl.BUJhYyXpUjLencYxQDM3VS6LqdwBHf90mPjpvYK13tCYPE-yMedgYl3jQJ3s-56wjKigIS-TR83xzy4lJOtOUPIuKhwvd1ZOTlXzYjaenaXMpheOqrkd-i9kgmcZR--xyVJD9pBN',
+    grant_type: 'authorization_code',
+    client_id: 'chd4jap4680m01b',
+    client_secret: 'tcdwbbyvhe182ed',
+});
+
+fetch('https://api.dropboxapi.com/2/auth/token/from_oauth1', {
+    method: 'POST',
+    body: params,
+})
+    .then((res) => res.json())
+    .then((data) => console.log('access data =>', data))
+    .catch((err) => console.log('access err =>', err));
